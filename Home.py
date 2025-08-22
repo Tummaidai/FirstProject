2eb48903e042b48f8530440b49a6655774c83573
Tummaidai <156872500+Tummaidai@users.noreply.github.com>
2025-08-22 12:17:52 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,7 +1,5 @@
 import streamlit as st
 
-
 st.page_link("pages/DTree.py", label="การทำนายข้อมูลด้วยเทคนิคต้นไม้ตัดสินใจ", icon="1️⃣")
 st.page_link("pages/KnnwithHeart.py", label="การทำนายข้อมูลด้วยเทคนิค Naive Baye", icon="2️⃣", disabled=False)
-
 st.page_link("pages/NaiveBaye.py", label="การทำนายข้อมูลด้วยเทคนิคต้นไม้ตัดสินใจ", icon="1️⃣")
