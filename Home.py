5e576ff59e66834e19f8caf86e26adaa5a9f90c4
Tummaidai <156872500+Tummaidai@users.noreply.github.com>
2025-09-25 22:07:11 +0700

    .
    .

--- Home.py
+++ Home.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
 st.page_link("pages/DTree.py", label="การทำนายข้อมูลด้วยเทคนิคต้นไม้ตัดสินใจ", icon="1️⃣")
-st.page_link("pages/KnnwithHeart.py", label="การทำนายข้อมูลด้วยเทคนิค Naive Baye", icon="2️⃣", disabled=False)
-st.page_link("pages/NaiveBaye.py", label="การทำนายข้อมูลด้วยเทคนิคต้นไม้ตัดสินใจ", icon="1️⃣")
+st.page_link("pages/NN.py", label="การทำนายข้อมูลด้วยเทคนิค Neural Network", icon="2️⃣", disabled=False)
+st.page_link("pages/Randomforest.py", label="การทำนายข้อมูลด้วยเทคนิคต้นไม้ตัดสินใจหลายต้น", icon="1️⃣")
